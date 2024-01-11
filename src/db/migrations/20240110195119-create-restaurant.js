ba13ec7707e8d375f0b9e052c73790d2846f0a5b
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-01-11 14:25:15 -0300

    feat(src/db/migrations): se agrega clave foránea  personalizada

---
+++ src/db/migrations/20240110195119-create-restaurant.js
@@ -0,0 +1,64 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Restaurants", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      payment_method: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      business_hour: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      tables_availability: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      delivery: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      web_page: {
+        type: Sequelize.STRING,
+      },
+      owner_email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: "Users",
+          key: "email",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Restaurants");
+  },
+};

4e1c0fd50efd648ab41541f5ab5891c530e11930
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-01-16 13:54:37 -0300

    style(src/server.js): archivo formateado por prettier

--- src/server.js
+++ src/server.js
@@ -1,31 +1,29 @@
-import { buildAPI } from "./api/index.js";
-import { db } from "./db/models/index.js";
+import { buildAPI } from './api.js'
+import { db } from './db/models/index.js'
 
 const getPort = () => {
-  const port = process.env.PORT || 3000;
-  return port;
-};
+  const port = process.env.PORT || 3000
+  return port
+}
 
 const startAPI = async (buildAPI, db) => {
   try {
-    const port = getPort();
-    const api = await buildAPI();
+    const port = getPort()
+    const api = await buildAPI()
 
-    await db.sequelize.authenticate();
-    console.log(
-      "Connection to the database has been established successfully."
-    );
+    await db.sequelize.authenticate()
+    console.log('Connection to the database has been established successfully.')
 
     api.listen(port, (err) => {
       if (err) {
-        return console.error("Failed", err);
+        return console.error('Failed', err)
       }
-      console.log(`Listening on port ${port}`);
-      return api;
-    });
+      console.log(`Listening on port ${port}`)
+      return api
+    })
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
-startAPI(buildAPI, db);
+startAPI(buildAPI, db)

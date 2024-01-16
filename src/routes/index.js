e7e0edbe2be6a9732753ae28882f14cef5046491
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-01-16 13:19:59 -0300

    refactor(src/routes/index.js): se quita el middleware errorHandler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,16 @@
-import Router from "koa-router";
-import { errorRouter } from "./errors/router.js";
-import { tokenRouter } from "./authentication/tokenRouter.js";
-import { authenticationRouter } from "./authentication/authenticationRouter.js";
-import { registerRouter } from "./user/registerRouter.js";
-import { userRouter } from "./user/router.js";
+import Router from 'koa-router'
+import { tokenRouter } from './authentication/tokenRouter.js'
+import { authenticationRouter } from './authentication/authenticationRouter.js'
+import { registerRouter } from './user/registerRouter.js'
+import { userRouter } from './user/router.js'
 
 const router = new Router({
-  prefix: "/api/v1",
-});
+  prefix: '/api/v1',
+})
 
-router.use(errorRouter.routes());
-router.use(registerRouter.routes());
-router.use(tokenRouter.routes());
-router.use(authenticationRouter.routes());
-router.use(userRouter.routes());
+router.use('/users/register', registerRouter.routes())
+router.use('/tokens', tokenRouter.routes())
+router.use(authenticationRouter.routes())
+router.use('/users', userRouter.routes())
 
-export { router as apiRouter };
+export { router as apiRouter }

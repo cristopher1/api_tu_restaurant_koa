cbd1d4d968b2ffdc974f13f77868f90825f923af
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-01-16 13:09:19 -0300

    style(src/config/jwt.js): cambio de snake case a camel case

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,6 +1,6 @@
-const { JWT_SECRET, JWT_ALGORITHM } = process.env;
+const { JWT_SECRET, JWT_ALGORITHM } = process.env
 
 export default {
-  jwt_secret: JWT_SECRET,
-  jwt_algorithm: JWT_ALGORITHM,
-};
+  JWTSecret: JWT_SECRET,
+  JWTAlgorithm: JWT_ALGORITHM,
+}
